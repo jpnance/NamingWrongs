b33513e3d395c4dbd8c76b5e575067a1d2316ec4
Patrick Nance <jpnance@gmail.com>
2016-12-02 14:54:10 -0800

    Holy nuts. Firefox extensions have the exact same specifications as Chrome extensions these days. This is great news. Sorry, Greasemonkey.

--- common/dictionary.sed
+++
@@ -1,2 +0,0 @@
-s/NamingWrongsDictionary.VERSION/"1.0"/
-s/NamingWrongsDictionary.UNQUOTED_VERSION/1.0/
--- gm/wrongs-gm.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name           Naming Wrongs
-// @namespace      http://coinflipper.org/
-// @description    Righting naming wrongs.
-// @include        http://www.espn.com/*
-// @include        http://www.coinflipper.org/*
-// @version        1.0
-// @version        NamingWrongsDictionary.UNQUOTED_VERSION
-// ==/UserScript==
