f4a6a3eede598ec7b70f50d0e39fb229af22faa4
Patrick Nance <jpnance@gmail.com>
2016-12-19 16:45:42 -0800

    This still needs a bit of work but this update externalizes a lot of data to a service. That should satisfy both the users and the Firefox add-on mods. Sites are up next.

--- wrongs.js
+++ wrongs.js
@@ -18,104 +18,43 @@ var sitePatterns = [
 	/www.ncaa.com/
 ];
 
-var wrongs = [
-	// Football Stadiums
-	[ /Sports Authority Field at Mile High/i, 'Mile High Stadium' ],
-	[ /Jones AT&T Stadium/i, 'Jones Stadium' ],
-	[ /BB&T Field/i, 'Groves Stadium' ],
-	[ /FirstEnergy Stadium/i, 'Cleveland Browns Stadium' ],
-	[ /Mercedes-Benz Superdome/i, 'Superdome' ],
-	[ /Qualcomm Stadium/i, 'Jack Murphy Stadium' ],
-	[ /AT&T Stadium/i, 'Cowboys Stadium' ],
-	[ /O\.co Coliseum/i, 'Oakland Coliseum' ],
-	[ /Hard Rock Stadium/i, 'Joe Robbie Stadium' ],
-	[ /New Era Field/i, 'Ralph Wilson Stadium' ],
-	[ /M&T Bank Stadium/i, 'Ravens Stadium' ],
-	[ /FedEx Field/i, 'Jack Kent Cooke Stadium' ],
-	[ /Camping World Stadium/i, 'Citrus Bowl' ],
-	[ /Reser Stadium/i, 'Parker Stadium' ],
-	[ /EverBank Field/i, 'Jacksonville Municipal Stadium' ],
-	[ /Albertsons Stadium/i, 'Bronco Stadium' ],
+function updateTheWrongs(root) {
+	var timestamp;
 
-	// Baseball Stadiums
-	[ /Progressive Field/i, 'Jacobs Field' ],
-	[ /Guaranteed Rate Field/i, 'Comiskey Park' ],
-	[ /UFCU Disch-Falk Field/i, 'Disch-Falk Field' ],
-	[ /Globe Life Park/i, 'The Ballpark in Arlington' ],
-	[ /US Cellular Field/i, 'Comiskey Park' ],
-	[ /Rogers Centre/i, 'SkyDome' ],
-	[ /Rogers Center/i, 'SkyDome' ],
+	chrome.storage.sync.get(['timestamp', 'wrongs'], function(item) {
+		var firstUpdate = false;
 
-	// Basketball Stadiums
-	[ /Moda Center/i, 'Rose Garden' ],
-	[ /BMO Harris Bradley Center/i, 'Bradley Center' ],
-	[ /Oracle Arena/i, 'Oakland Coliseum Arena' ],
-	[ /Quicken Loans Arena/i, 'Gund Arena' ],
-	[ /Smoothie King Center/i, 'New Orleans Arena' ],
-	[ /Amalie Arena/i, 'The Ice Palace' ],
-	[ /Bridgestone Arena/i, 'Nashville Arena' ],
-	[ /Gila River Arena/i, 'Glendale Arena' ],
-	[ /PNC Arena/i, 'Raleigh Entertainment & Sports Arena' ],
-	[ /Scotiabank Saddledome/i, 'Olympic Saddledome' ],
-	[ /SAP Center/i, 'San Jose Arena' ],
-	[ /SAP Center at San Jose/i, 'San Jose Arena' ],
-	[ /Scottrade Center/i, 'Kiel Center' ],
-	[ /EagleBank Arena/i, 'Patriot Center' ],
-	[ /XL Center/i, 'Hartford Civic Center' ],
-	[ /Dunkin' Donuts Center/i, 'Providence Civic Center' ],
-	[ /Exactech Arena at Stephen O'Connell Center/i, 'Stephen O\'Connell Center' ],
-	[ /Exactech Arena at O'Connell Center/i, 'O\'Connell Center' ],
-	[ /CFE Arena/i, 'UCF Arena' ],
-	[ /Watsco Center/i, 'University of Miami Convocation Center' ],
-	[ /State Farm Center/i, 'Assembly Hall' ],
-	[ /Alaska Airlines Arena at Hec Edmundson Pavilion/i, 'Hec Edmundson Pavilion' ],
+		if (!item.timestamp) {
+			timestamp = Date.now();
+			firstUpdate = true;
+		}
+		else {
+			timestamp = item.timestamp;
+		}
 
-	// College Bowl Games
-	[ /Air Force Reserve Celebration Bowl/i, 'Celebration Bowl' ], // *
-	[ /Gildan New Mexico Bowl/i, 'New Mexico Bowl' ],
-	[ /Las Vegas Bowl presented by Geico/i, 'Las Vegas Bowl' ],
-	[ /Raycom Media Camellia Bowl/i, 'Camellia Bowl' ], // *
-	[ /AutoNation Cure Bowl/i, 'Cure Bowl' ], // *
-	[ /R\+L Carriers New Orleans Bowl/i, 'New Orleans Bowl' ],
-	[ /San Diego County Credit Union Poinsettia Bowl/i, 'Poinsettia Bowl' ],
-	[ /Famous Idaho Potato Bowl/i, 'Humanitarian Bowl' ],
-	[ /Popeyes Bahamas Bowl/i, 'Bahamas Bowl' ], // *
-	[ /Lockheed Martin Armed Forces Bowl/i, 'Armed Forces Bowl' ],
-	[ /Dollar General Bowl/i, 'Mobile Alabama Bowl' ], // *
-	[ /Quick Lane Bowl/i, 'Motor City Bowl' ], // *
-	[ /Camping World Independence Bowl/i, 'Independence Bowl' ],
-	[ /Zaxby's Heart of Dallas Bowl/i, 'Heart of Dallas Bowl' ],
-	[ /Military Bowl Presented by Northrop Grumman/i, 'Military Bowl' ],
-	[ /National Funding Holiday Bowl/i, 'Holiday Bowl' ],
-	[ /Motel 6 Cactus Bowl/i, 'Cactus Bowl' ],
-	[ /New Era Pinstripe Bowl/i, 'Pinstripe Bowl' ],
-	[ /Russell Athletic Bowl/i, 'Tangerine Bowl' ], // *
-	[ /Foster Farms Bowl/i, 'San Francisco Bowl' ], // *
-	[ /AdvoCare V100 Texas Bowl/i, 'Texas Bowl' ],
-	[ /Belk Bowl/i, 'Queen City Bowl' ],
-	[ /Valero Alamo Bowl/i, 'Alamo Bowl' ],
-	[ /AutoZone Liberty Bowl/i, 'Liberty Bowl' ],
-	[ /Hyundai Sun Bowl/i, 'Sun Bowl' ],
-	[ /Franklin American Mortgage Music City Bowl/i, 'Music City Bowl' ],
-	[ /Nova Home Loans Arizona Bowl/i, 'Arizona Bowl' ],
-	[ /Capital One Orange Bowl/i, 'Orange Bowl' ],
-	[ /Buffalo Wild Wings Citrus Bowl/i, 'Citrus Bowl' ],
-	[ /TaxSlayer Bowl/i, 'Gator Bowl' ],
-	[ /Outback Bowl/i, 'Hall of Fame Bowl' ],
-	[ /Goodyear Cotton Bowl Classic/i, 'Cotton Bowl' ],
-	[ /Rose Bowl Game presented by Northwestern Mutual/i, 'Rose Bowl' ],
-	[ /Allstate Sugar Bowl/i, 'Sugar Bowl' ],
-	[ /Chick-fil-A Peach Bowl/i, 'Peach Bowl' ],
-	[ /PlayStation Fiesta Bowl/i, 'Fiesta Bowl' ],
+		var timeSinceLastUpdate = (Date.now() - timestamp) / 1000;
 
-	// Old College Bowl Games
-	[ /Ticket City Bowl/i, 'Heart of Dallas Bowl' ],
-	[ /Capital One Bowl(?! Mania)/i, 'Citrus Bowl' ],
-	[ /BattleFrog Fiesta Bowl/i, 'Fiesta Bowl' ],
-	[ /Royal Purple Las Vegas Bowl/i, 'Las Vegas Bowl' ]
-];
+		if (timeSinceLastUpdate > 10 || firstUpdate) {
+			chrome.storage.sync.set({ timestamp: Date.now() }, function() {
+				var xhr = new XMLHttpRequest();
+				xhr.onreadystatechange = function(data) {
+					if (xhr.readyState == 4) {
+						if (xhr.status == 200) {
+							var data = JSON.parse(xhr.responseText);
+							rightTheWrongs(root, data.wrongs);
+						}
+					}
+				};
 
-function rightTheWrongs(root) {
+				var url = 'http://coinflipper.org/~jpnance/wrongs.json?_=' + Math.random();
+				xhr.open('GET', url, true);
+				xhr.send();
+			});
+		}
+	});
+}
+
+function rightTheWrongs(root, wrongs) {
 	var iterator = document.createNodeIterator(
 		root,
 		NodeFilter.SHOW_TEXT,
@@ -133,10 +72,13 @@ function rightTheWrongs(root) {
 	while (node = iterator.nextNode()) {
 		for (var i = 0; i < wrongs.length; i++) {
 			var wrong = wrongs[i];
+			var regex = new RegExp(wrong[0]);
 
-			if (node.nodeValue.match(wrong[0])) {
-				node.nodeValue = node.nodeValue.replace(wrong[0], wrong[1]);
+			if (node.nodeValue.match(regex)) {
+				node.nodeValue = node.nodeValue.replace(regex, wrong[1]);
 			}
+
+			delete regex;
 		}
 	}
 
@@ -152,11 +94,11 @@ function matchesHref(pattern) {
 if (sitePatterns.some(matchesHref)) {
 	var observer = new MutationObserver(function(mutations, observer) {
 		mutations.forEach(function(mutation) {
-			rightTheWrongs(mutation.target);
+			updateTheWrongs(mutation.target);
 		});
 	});
 
 	observer.observe(document.body, { childList: true, subtree: true });
 
-	rightTheWrongs(document.body);
+	updateTheWrongs(document.body);
 }
